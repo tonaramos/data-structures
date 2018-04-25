e8658c8e3f71acd0d410df2eb3038668b12a93e0
Chrystal Zou <chrystalzou@gmail.com>
2018-04-25 14:44:33 -0700

    Passed tests for The functional pattern: stack

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -3,15 +3,22 @@ var Stack = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var array = [];
+  var size = array.length;
 
   // Implement the methods below
   someInstance.push = function(value) {
+    array.push(value);
+    return array.length;
   };
 
   someInstance.pop = function() {
+    // does not error when removing from empty stack
+    return array.pop();
   };
 
   someInstance.size = function() {
+    return array.length;
   };
 
   return someInstance;
