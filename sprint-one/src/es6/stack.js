78f761a430af9511132be51f1c925c6b3f365e4a
Chrystal Zou <chrystalzou@gmail.com>
2018-04-26 12:51:37 -0700

    Passed ES6 stack tests

--- sprint-one/src/es6/stack.js
+++ sprint-one/src/es6/stack.js
@@ -2,6 +2,29 @@ class Stack {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+  	this.num = 0;
+  }
+
+  push(value) {
+	this.num++;
+    this[this.num] = value;  	
+  }
+
+  pop() {
+	if (this.num === 0) {
+	  return null;
+	} else {
+		var deleted = this[this.num];
+		delete this[this.num];
+		this.num--;
+		return deleted;
+	}  	
+  }
+
+  size() {
+  	return this.num;
   }
 
 }
+
+var stack = new Stack();
