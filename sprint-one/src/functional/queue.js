55fb94bd068f052b14251b70551c721581c7e4c4
Chrystal Zou <chrystalzou@gmail.com>
2018-04-25 17:35:00 -0700

    Passed tests in stack.js; passed tests in queue.js except the last one.

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,16 +3,30 @@ var Queue = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
-
+  var num = 0;
+  // add a variable to keep track of the oldest element each time enqueue is called
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    storage[num] = value;
+    num++;
+    // increment the oldest variable counter
   };
 
   someInstance.dequeue = function() {
+    if (num === 0) {
+      return null;
+    } else {
+      num--;
+      // delete the oldest element key-value pair with the lowest oldest variable
+      var deleted = storage[Object.keys(storage)[0]];
+      delete storage[Object.keys(storage)[0]];
+      return deleted;
+    }
   };
 
   someInstance.size = function() {
+    return _.size(storage);
   };
 
   return someInstance;
--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -3,22 +3,33 @@ var Stack = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
-  var array = [];
-  var size = array.length;
+  // create a variable to keep track of each key-value pair in the storage object
+  var num = 0;
 
   // Implement the methods below
   someInstance.push = function(value) {
-    array.push(value);
-    return array.length;
+    // create a new key-value pair in storage object;
+    storage[num] = value;
+    num++;
   };
 
   someInstance.pop = function() {
-    // does not error when removing from empty stack
-    return array.pop();
+    // delete last property in an object
+    // declare variable to hold the item you're about to delete
+    if (num === 0) {
+      return null;
+    } else {
+      num--;
+      var deleted = storage[num];
+      // delete storage[num]
+      delete storage[num];
+      // return the variable representing the item you just deleted
+      return deleted;
+    }
   };
 
   someInstance.size = function() {
-    return array.length;
+    return num;
   };
 
   return someInstance;
