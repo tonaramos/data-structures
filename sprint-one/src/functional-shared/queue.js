102d9ce6a37c3c9593ba10114630fc912d2ec4fd
Chrystal Zou <chrystalzou@gmail.com>
2018-04-26 10:50:49 -0700

    Passed tests for functional-shared queue and stack

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,8 +1,32 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var queueObj = {
+  	num : 0
+  };
+  _.extend(queueObj, queueMethods);
+  return queueObj;
 };
 
-var queueMethods = {};
+var queueMethods = {
+  enqueue : function(value) {
+    this[this.num] = value;
+    this.num++;
+  },
+  dequeue : function() {
+   if (this.num === 0) {
+   	return 0;
+   } else {
+	   var firstItem = this[Object.keys(this)[0]];
+	   var firstKey = Object.keys(this)[0];
+	   delete this[firstKey];
+	   this.num--;
+	   return firstItem;
+	}
+  },
+  size : function() {
+    return this.num;
+  }
+};
 
-
+var queue = Queue();
--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,32 @@
-var Stack = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+var Stack = function() { // constructor function
+  var stackObj = {
+    num : 0
+  };
+  _.extend(stackObj, stackMethods);
+  return stackObj; // an instance of the Stack class of objects
 };
 
-var stackMethods = {};
+var stackMethods = {
+  // methods go in here
+  push: function(value) {
+    // should create a new key-value pair in the storage object
+    this.num++;
+    this[this.num] = value;
+    // increment num;
+  },
+  pop: function() {
+    if (this.num === 0) {
+      return null;
+    } else {
+      var deleted = this[this.num];
+      delete this[this.num];
+      this.num--;
+      return deleted;
+    }
+  },
+  size: function() {
+    return this.num;
+  }
+};
 
-
+var stack = Stack(); // creates new object stackObj;
