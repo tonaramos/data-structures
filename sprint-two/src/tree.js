d7d94ab93812c24dbc21c89ece2b0332d66dd06a
Chrystal Zou <chrystalzou@gmail.com>
2018-04-27 15:52:57 -0700

    Passed all tests for tree.js, and added time complexities for each function

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -2,22 +2,44 @@ var Tree = function(value) {
   var newTree = {};
   newTree.value = value;
 
-  // your code here
-  newTree.children = null;  // fix me
-
+  // your code here	
+  newTree.children = [];  // fix me
+  _.extend(newTree, treeMethods)
   return newTree;
 };
 
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+	// create new node
+	var newNode = Tree(value);
+	this.children.push(newNode);
+	// push to children array
 };
 
 treeMethods.contains = function(target) {
+	var output = false;
+
+	function checkChildren(node, target) {
+		for (var i = 0; i < node.length; i++) {
+			if (node[i].value === target) {
+				output = true;
+			}
+		checkChildren(node[i].children, target);
+		}
+	}
+
+	if (this.value === target) {
+		output = true;
+	}
+	checkChildren(this.children, target);
+	return output;
 };
 
-
+var tree = Tree();
 
 /*
  * Complexity: What is the time complexity of the above functions?
+ treeMethods.addChild: O(1);
+ treeMthods.contains: O(n);
  */
